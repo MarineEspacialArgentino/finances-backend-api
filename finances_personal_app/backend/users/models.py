772cbf074f44db5fae19a657b5d146bbcef05c24
Gonzalo Lopez <lopezgonzaloariel1@gmail.com>
2025-12-29 00:08:34 -0300

    Remove redundant finances_personal_app directory

--- finances_personal_app/backend/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    
-    # el AbstractUser ya tiene campos como username, password, etc.
-    
-    def __str__(self):
-        return self.username
